3b75e497966f1aaa4e8cbef43f32cf0e2b1aa721
rherms <rlhermstein@yahoo.com>
2015-08-12 21:58:41 -0400

    Started training data. Modified python script to also calculate kill participation, percentage of team's damage, and percentage of team's gold.

--- processMatches.py
+++ processMatches.py
@@ -71,25 +71,6 @@ def getChampName(champId):
 		return "Error"
 	else:
 		return champDict[champId]
-	'''staticVersion = "1.2"
-	region = "na"
-	apiKey = loadAPIKey()
-	url = "https://na.api.pvp.net/api/lol/static-data/na/v" + staticVersion + "/champion/" + champId + "?api_key=" + apiKey;
-	try:
-		content = urllib2.urlopen(url)
-	except urllib2.HTTPError, e:
-		if(e.code != 200):
-			print("Error in API request for getChampName :(")
-			if(e.code == 429): # too many requests
-				time.sleep(1) # sleep a second before trying again
-				return getChampName(champId)
-			else:
-				return "Error"
-
-	response = str(content.read()) # str() so that it is not unicode
-	# convert response to json
-	response = json.loads(response)
-	return str(response["name"])'''
 
 # Given an id, gets the item's name.
 def getItemName(itemId):
@@ -97,74 +78,52 @@ def getItemName(itemId):
 		return "Error"
 	else:
 		return itemDict[itemId]
-	'''if(itemId == "0"):
-		return ""
-	staticVersion = "1.2"
-	region = "na"
-	apiKey = loadAPIKey()
-	url = "https://na.api.pvp.net/api/lol/static-data/na/v" + staticVersion + "/item/" + itemId + "?api_key=" + apiKey;
-	
-	try:
-		content = urllib2.urlopen(url)
-	except urllib2.HTTPError, e:
-		if(e.code != 200):
-			print("Error in API request for getItemName :(")
-			if(e.code == 429): # too many requests
-				time.sleep(1) # sleep a second before trying again
-				return getItemName(itemId)
-			else:
-				return "Error"
-
-	response = str(content.read()) # str() so that it is not unicode
-	# convert response to json
-	response = json.loads(response)
-	return str(response["name"])'''
 
 def generateTrainingData(players, matchId):
-	with open("matchStats.csv", "w") as f:
-		f.write("Index,Champion,Kills,Deaths,Assists,KDA,Gold,Damage,avgKdaDiff,avgGoldDiff,avgDamageDiff") #,kdaDiff4,kdaDiff5,kdaDiff6,kdaDiff7,kdaDiff8,kdaDiff9,\
-			     #goldDiff1,goldDiff2,goldDiff3,goldDiff4,goldDiff5,goldDiff6,goldDiff7,goldDiff8,goldDiff9,\
-			     #damageDiff1,damageDiff2,damageDiff3,damageDiff4,damageDiff5,damageDiff6,damageDiff7,damageDiff8,damageDiff9")
+	with open("matchStats" + matchId + ".csv", "w") as f:
+		f.write("Index,Champion,Kills,Deaths,Assists,KDA,Gold,Damage,avgKdaDiff,avgGoldDiff,avgDamageDiff,killPart,damagePerc,goldPerc")
 		f.write("\n")
 		for i in range(0, len(players)):
 			player = players[i]
 			f.write(str(i) + "," + player["championName"] + "," + str(player["kills"]) + "," + str(player["deaths"]) + "," + str(player["assists"]) + "," \
 					+ str(player["kda"]) + "," + str(player["goldEarned"]) + "," + str(player["totalDamage"]) + "," \
-					+ str(player["avgKdaDiff"]) + "," + str(player["avgGoldDiff"]) + "," + str(player["avgDamageDiff"]))
-					#+ str(player["kdaDiff1"]) + "," + str(player["kdaDiff2"]) + "," + str(player["kdaDiff3"]) + "," + str(player["kdaDiff4"]) + "," \
-					#+ str(player["kdaDiff5"]) + "," + str(player["kdaDiff6"]) + "," + str(player["kdaDiff7"]) + "," + str(player["kdaDiff8"]) + "," + str(player["kdaDiff9"]) + "," \
-					#+ str(player["goldDiff1"]) + "," + str(player["goldDiff2"]) + "," + str(player["goldDiff3"]) + "," + str(player["goldDiff4"]) + "," \
-					#+ str(player["goldDiff5"]) + "," + str(player["goldDiff6"]) + "," + str(player["goldDiff7"]) + "," + str(player["goldDiff8"]) + "," + str(player["goldDiff9"]) + "," \
-					#+ str(player["damageDiff1"]) + "," + str(player["damageDiff2"]) + "," + str(player["damageDiff3"]) + "," + str(player["damageDiff4"]) + "," \
-					#+ str(player["damageDiff5"]) + "," + str(player["damageDiff6"]) + "," + str(player["damageDiff7"]) + "," + str(player["damageDiff8"]) + "," + str(player["damageDiff9"]))
+					+ str(player["avgKdaDiff"]) + "," + str(player["avgGoldDiff"]) + "," + str(player["avgDamageDiff"]) + "," + str(player["killPart"])
+					+ "%," + str(player["damagePerc"]) + "%," + str(player["goldPerc"]) + "%")
 			f.write("\n")
 
 	while(True):
 		index = raw_input("Index of someone who carried (negative number or enter to stop): ")
-		if(index == "" or int(index) < 0):
+		if(index == ""):
 			break
+		try:
+			if(int(index) < 0):
+				break
+		except ValueError:
+			print("Invalid index. Please try again.")
+			continue	
 		index = int(index)
 		verified = verifyInput(index)
 		if(verified):
 			print("Player at index " + str(index) + " marked as a carry.")
 			player = players[index]
 			player["carried"] = True
-			appendToFile("carryPlayers.json", str(player) + ",")
 		else:
 			print("Player at index " + str(index) + " NOT marked as a carry.")		
 
 	for player in players:
-		appendToFile("trainingData.json", str(player) + ",")
+		appendToFile("trainingData.json", str(player) + ",") # remove this after model is trained
+		if(player["carried"]):
+			appendToFile("carryPlayers.json", str(player) + ",")
 
-	exit() #uncomment this when ready for real
-	appendToFile("processedMatches", matchId)
+	#exit() #comment this when ready for real
+	appendToFile("processedMatches.txt", matchId)
 
 def processMatch(matchId):
 	region = "na"
 	matchVersion = "2.2"
 	apiKey = loadAPIKey()
 	url = "https://na.api.pvp.net/api/lol/" + region + "/v" + matchVersion + "/match/" + matchId + "?api_key=" + apiKey
-	
+
 	# simple API request to get response as plaintext
 	try:
 		content = urllib2.urlopen(url)
@@ -189,15 +148,21 @@ def processMatch(matchId):
 
 	players = [] # our array to populate
 	participants = response["participants"]
+	totalKills100 = 0
+	totalKills200 = 0
+	totalDamage100 = 0
+	totalDamage200 = 0
+	totalGold100 = 0
+	totalGold200 = 0
 	for participant in participants:
 		# initialize with fields that are same for every player
 		player = {"region": str(region), "gameMode": str(gameMode), "patch": str(patch), "matchId": str(matchId)}
 		player["championId"] = str(participant["championId"])
 		stats = participant["stats"]
-		player["kills"] = str(stats["kills"])
-		player["assists"] = str(stats["assists"])
-		player["deaths"] = str(stats["deaths"])
-		player["kda"] = round(float(player["kills"]) + float(player["assists"]) / 3.0 / float(player["deaths"]), 2) # our own kda where assists are worth one third of kills
+		player["kills"] = int(stats["kills"])
+		player["assists"] = int(stats["assists"])
+		player["deaths"] = int(stats["deaths"])
+		player["kda"] = round((float(player["kills"]) + float(player["assists"]) / 2.0) / float(player["deaths"]), 2) # our own kda where assists are worth one third of kills
 		player["goldEarned"] = int(stats["goldEarned"])
 		player["item0"] = str(stats["item0"])
 		player["item1"] = str(stats["item1"])
@@ -209,10 +174,31 @@ def processMatch(matchId):
 		player["magicDamage"] = int(stats["magicDamageDealtToChampions"])
 		player["physDamage"] = int(stats["physicalDamageDealtToChampions"])
 		player["totalDamage"] = int(stats["totalDamageDealtToChampions"])
+		player["teamId"] = int(participant["teamId"])
+		if(player["teamId"] == 100):
+			totalKills100 += player["kills"]
+			totalGold100 += player["goldEarned"]
+			totalDamage100 += player["totalDamage"]
+		elif(player["teamId"] == 200):
+			totalKills200 += player["kills"]
+			totalGold200 += player["goldEarned"]
+			totalDamage200 += player["totalDamage"]
+		else:
+			print("Unknown team id in processMatch: " + str(player["teamId"]))
 		player["carried"] = False # will be changed in the machine learning part
 		players.append(player)
 
 	for player in players:
+		if(player["teamId"] == 100):
+			player["killPart"] = int(round(float(player["kills"] + player["assists"]) / totalKills100, 2) * 100)
+			player["damagePerc"] = int(round(float(player["totalDamage"]) / totalDamage100, 2) * 100)
+			player["goldPerc"] = int(round(float(player["goldEarned"]) / totalGold100, 2) * 100)
+		elif(player["teamId"] == 200):
+			player["killPart"] = int(round(float(player["kills"] + player["assists"]) / totalKills200, 2) * 100)
+			player["damagePerc"] = int(round(float(player["totalDamage"]) / totalDamage200, 2) * 100)
+			player["goldPerc"] = int(round(float(player["goldEarned"]) / totalGold200, 2) * 100)
+
+
 		goldEarned = player["goldEarned"]
 		kda = player["kda"]
 		damage = player["totalDamage"]
@@ -240,6 +226,7 @@ def processMatch(matchId):
 
 if __name__ == "__main__":
 	processedMatches = loadProcessedMatches()
+	print("You have trained for " + str(len(processedMatches)) + " matches already! :)")
 
 	with open(sys.argv[1]) as f:
 		for line in f:
@@ -251,3 +238,5 @@ if __name__ == "__main__":
 			if(not matchId in processedMatches):
 				print("Advancing to match: " + matchId)
 				processMatch(matchId)
+			else:
+				print("Skipping over match: " + matchId + " because it was already processed.")
